a079fa30b64e61b1d6373691050d88515d370b50
8ma3 <syaorann200614567@gmail.com>
2016-01-07 10:29:29 +0900

    add jibun

--- mainPhotoShare/PhotoShare/LoginViewController.swift
+++ mainPhotoShare/PhotoShare/LoginViewController.swift
@@ -18,11 +18,11 @@ class LoginViewController: FormViewController {
         form +++= Section()
             <<< EmailRow("email") {
                 $0.title = "Email"
-                $0.value = "t.k.kanata.kitto@gmail.com"
+                $0.value = "syaorann200614567@gmail.com"
             }
             <<< PasswordRow("password"){
                 $0.title = "Password"
-                $0.value = "eccecc"
+                $0.value = "43215632"
             }
             <<< ButtonRow("submit") {
                 $0.title = "LOGIN"
